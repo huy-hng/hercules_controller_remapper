2937e5b6efd60718b7d6f3909a2b881653545793
Huy <huy@hoang.dev>
2023-12-10 12:15:50 +0100

    rename file

--- main.py
+++ main.py
@@ -1,66 +1,49 @@
-# %%
 import xml.etree.ElementTree as ET
-# %%
+
+MAP_FILE = './custom/custom_map.xml'
+CONTROL_FILE =  './custom/custom_control.xml'
+
+INPUT_FILE = './midi_templates/Hercules DJ Inpulse 500_31.djm'
+OUTPUT_FILE = 'Hercules DJ Inpulse 500 - Custom.djm'
+
+def find_child_by_name(element, name):
+	for child in element:
+		if child.attrib['name'] == name:
+			return child
+	return None
+
+def replace_element(parent, custom_element_file):
+	custom_element = ET.parse(custom_element_file)
+	custom_element = custom_element.getroot()
+
+	for child in custom_element:
+		custom_child_name = child.attrib['name']
+		og_child = find_child_by_name(parent, custom_child_name)
+
+		if og_child is not None:
+			parent.remove(og_child)
+
+		parent.append(child)
 
 def main():
-	# def get_tree(path)
-	file_name = './midi_files/Hercules DJ Inpulse 500 copy 2.djm'
+	file_name = INPUT_FILE
 	tree = ET.parse(file_name)
 	root = tree.getroot()
 
 	midi_map = root.find('midi-map')
-	if midi_map is None:
+	midi_device = root.find('midi-device')
+	if midi_map is None or midi_device is None:
+		print('sth wrong fam')
 		return
+	# root.remove(midi_map)
 
+	# default_midi_map = ET.parse('./custom/default_map.html')
+	# root.append(default_midi_map.getroot())
 
-	for child in midi_map:
-		attribs = child.attrib
-		# print(attribs['name'])
-		name = attribs['name']
-		if name.startswith('BEATJMP'):
-			num = name[-3]
-			deck = name[-1]
-			
-			change_value_in_action_attrib(child, 'value', '"-"')
+	replace_element(midi_map, MAP_FILE)
+	replace_element(midi_device, CONTROL_FILE)
 
-			# if deck == 'A':
-			# 	attribs['action'] = beatjump_text(1, -12)
-
-		# value = attribs['value']
-		# value = '3'
-		# for attrib in child.attrib:
-			# print(attrib.)
-
-	file_name_2 = 'Test.djm'
-	# print(midi_map[0].attrib)
-	tree.write('./midi_files/' + file_name_2)
-
-
-def beatjump_text(channel, beats):
-	return f'chann={channel}  action=skip_beat value="{beats}"'
-
-def change_value_in_action_attrib(element: ET.Element, target_attrib: str, new_val: str):
-	# print(id(element))
-	attribs = element.attrib
-	action_attribs = attribs['action'].split(' ')
-	for i, attrib in enumerate(action_attribs):
-		if attrib.startswith(target_attrib):
-			attrib = attrib.split('=')
-			attrib[1] = new_val
-			attrib = '='.join(attrib)
-			action_attribs[i] = attrib
-
-	element.attrib['action'] = ' '.join(action_attribs)
-
-
-
-def find_name(branch: ET.Element, name: str):
-	for child in branch:
-		attribs = child.attrib
-		if name == attribs['name']:
-			return child
-
-
+    tree.write(OUTPUT_FILE)
 
 if __name__ == '__main__':
 	main()
